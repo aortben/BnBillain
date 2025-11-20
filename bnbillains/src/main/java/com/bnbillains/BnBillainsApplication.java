fb5c9ef00777fabfc6d76e615e5dc0b4e46a39d0
Arvarisu <alvaroortegabenite03@gmail.com>
2025-11-20 15:32:22 +0100

    Esqueleto inicial del proyecto

---
+++ bnbillains/src/main/java/com/bnbillains/BnBillainsApplication.java
@@ -0,0 +1,13 @@
+package com.bnbillains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnBillainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnBillainsApplication.class, args);
+	}
+
+}
---
+++ bnbillains/src/test/java/com/bnbillains/BnBillainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnbillains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnBillainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
