1c393b83869dacadf4e1d1ffd4789aa402d1b28c
aortben <alvaroortegabenitez03@gmail.com>
2025-11-24 12:28:28 +0100

    Factura Repository

---
+++ bnbillains/src/main/java/com/bnbillains/repositories/FacturaRepository.java
@@ -0,0 +1,21 @@
+package com.bnbillains.repositories;
+
+import com.bnbillains.entities.Factura;
+import com.bnbillains.entities.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FacturaRepository extends JpaRepository<Factura, Long> {
+
+    Optional<Factura> findByReserva(Reserva reserva);
+    Optional<Factura> findByReserva_Id(Long reservaId);
+    List<Factura> findByFechaEmision(LocalDate fechaEmision);
+    List<Factura> findByMetodoPagoContainingIgnoreCase(String metodo);
+    boolean existsByReserva_Id(Long reservaId);
+}
+
