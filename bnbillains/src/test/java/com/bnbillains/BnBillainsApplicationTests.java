fa8c4548a30541fc7beaf318dba297c5b00f05f1
aortben <alvaroortegabenitez03@gmail.com>
2025-11-24 13:38:37 +0100

    Añadidos los htmls relciones a guarida

--- bnbillains/src/test/java/com/bnbillains/BnBillainsApplicationTests.java
+++ bnbillains/src/test/java/com/bnbillains/BnBillainsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bnbillains;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BnBillainsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bnbillains;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BnBillainsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
