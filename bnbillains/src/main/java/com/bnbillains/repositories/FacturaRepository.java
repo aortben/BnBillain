a8eeaeabb5ca53517d03d780b7e2c14731b9f1bd
Gamer0-0-0-0 <gamer0jes@gmail.com>
2025-11-28 20:14:44 +0100

    Añadiendo metodos faltantes al repositorio de factura

--- bnbillains/src/main/java/com/bnbillains/repositories/FacturaRepository.java
+++ bnbillains/src/main/java/com/bnbillains/repositories/FacturaRepository.java
@@ -2,6 +2,7 @@ package com.bnbillains.repositories;
 
 import com.bnbillains.entities.Factura;
 import com.bnbillains.entities.Reserva;
+import com.bnbillains.entities.Villano;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,8 @@ public interface FacturaRepository extends JpaRepository<Factura, Long> {
 
     Optional<Factura> findByReserva(Reserva reserva);
     Optional<Factura> findByReserva_Id(Long reservaId);
+    Optional<Factura> findByVillano(Villano villano);
+    Optional<Factura> findByVillano_Id(Long villanoId);
     List<Factura> findByFechaEmision(LocalDate fechaEmision);
     List<Factura> findByMetodoPagoContainingIgnoreCase(String metodo);
     boolean existsByReserva_Id(Long reservaId);
