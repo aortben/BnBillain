f35c3b63a61e4c1fcd689a334dc627678ec9f27f
lopeconlacope13 <alopper.daw@gmail.com>
2025-12-01 13:15:58 +0100

    implementacion CRUD completa con paginacion, filtros y ordenacion

--- src/main/java/com/bnbillains/repositories/ComodidadRepository.java
+++ src/main/java/com/bnbillains/repositories/ComodidadRepository.java
@@ -1,6 +1,7 @@
 package com.bnbillains.repositories;
 
 import com.bnbillains.entities.Comodidad;
+import org.springframework.data.domain.Sort; // IMPORTANTE
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +9,14 @@ import java.util.List;
 
 @Repository
 public interface ComodidadRepository extends JpaRepository<Comodidad, Long> {
+
     Comodidad findByNombre(String nombre);
     Comodidad findByNombreIgnoreCase(String nombre);
+
     List<Comodidad> findByNombreContainingIgnoreCase(String fragmento);
+
+    // busqueda y ordenacion
+    List<Comodidad> findByNombreContainingIgnoreCase(String fragmento, Sort sort);
+
     boolean existsByNombreIgnoreCase(String nombre);
 }
-
