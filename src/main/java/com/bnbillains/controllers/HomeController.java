3049cf6137413f55a1be0f10c24f84ae91aced29
Alvaro Lopez <alopper.daw@gmail.com>
2025-12-08 22:38:55 +0100

    comentario HomeController

--- src/main/java/com/bnbillains/controllers/HomeController.java
+++ src/main/java/com/bnbillains/controllers/HomeController.java
@@ -3,10 +3,21 @@ package com.bnbillains.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controlador raíz de la aplicación.
+ * Gestiona la página de bienvenida (Landing Page).
+ */
 @Controller
 public class HomeController {
+
+    /**
+     * Maneja la petición a la raíz del sitio ("/")
+     * @return La vista "index" (templates/index.html)
+     */
     @GetMapping("/")
     public String home(Model model) {
+        // Aquí se podrían añadir atributos de bienvenida si fuera necesario
         return "index";
     }
 }
